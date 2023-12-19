979f2447aefecf57e21e8b363421cf4a6808a109
phamtienanh01 <Phamtienanh0@gmail.com>
2023-12-19 21:49:38 +0700

    nhatnguyen

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import './App.css';
+import Form from './components/Form';
+import UserList from './components/UserList';
+function App() {
+  return (
+    <div className="App">
+      <header>
+         <h1>React App</h1>
+        <Form/>
+        <UserList/>
+      </header>
+    </div>
+  );
+}
+
+export default App;
